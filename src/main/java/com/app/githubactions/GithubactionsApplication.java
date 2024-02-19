1f4b6590d5ae034825089e767c93c8372c808db8
C0SS10 <estebancg12@hotmail.com>
2024-02-19 11:36:54 -0500

    Add report generation functionality 🧰

--- src/main/java/com/app/githubactions/GithubactionsApplication.java
+++ src/main/java/com/app/githubactions/GithubactionsApplication.java
@@ -1,13 +1,24 @@
 package com.app.githubactions;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.app.githubactions.utils.ReporteController;
+
 @SpringBootApplication
 public class GithubactionsApplication {
+    @Autowired
+    ReporteController reporteController;
 
     public static void main(String[] args) {
         SpringApplication.run(GithubactionsApplication.class, args);
+    }
+
+    public void run(String[] args) {
+        if (args.length > 0 && args[0].equals("generar-reporte")) {
+            reporteController.generarReporte();
+        }
     }
 
 }
