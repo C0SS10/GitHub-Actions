5a54feb5f78de226cfce3981348986c5cdc571c9
Cossio <estebancogo47@gmail.com>
2024-02-18 19:08:06 -0500

    Refactor main method and RegistroController 🧰

--- src/main/java/com/app/githubactions/GithubactionsApplication.java
+++ src/main/java/com/app/githubactions/GithubactionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubactionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubactionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionsApplication.class, args);
+    }
 
 }
