169cd282ab0426fabce3b77198ad717b3057e225
Esteban <estebancg12@hotmail.com>
2024-02-25 10:51:41 -0500

    Add component scan annotation 🧰

--- src/main/java/com/app/githubactions/GithubactionsApplication.java
+++ src/main/java/com/app/githubactions/GithubactionsApplication.java
@@ -3,10 +3,12 @@ package com.app.githubactions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.app.githubactions.utils.ReporteController;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.app.githubactions"})
 public class GithubactionsApplication {
     @Autowired
     ReporteController reporteController;
